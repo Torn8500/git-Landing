cf860806a25083374965abc32f4607433ae71aca
Torn8500 <quangpm.it@gmail.com>
2023-03-07 17:53:30 +0700

    start

---
+++ land/pl-ketomatcha/1/js/pl27.js
@@ -0,0 +1,19 @@
+(function () {
+	'use strict';
+
+	window.countryList = {
+		pl: {
+			name: 'Polska',
+			code: 'pl',
+			price: 155,
+			oldPrice: 310,
+			labelPrice: 'zł',
+			phoneHelper: 'Na przykład, +48 123456789',
+			nameHelper: 'Tom Czerniak',
+			nameError: 'Imię (wymagane)',
+			phoneError: 'Błędnie podany numer',
+			countryError: 'Kraj (wymagany)',
+			isDefault: true,
+		},
+	};
+})();
