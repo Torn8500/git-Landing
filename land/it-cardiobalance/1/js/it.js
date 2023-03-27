a3a5951aaaaf1569437af6baf4673478be0d4bb3
Torn8500 <quangpm.it@gmail.com>
2023-03-27 10:27:30 +0700

    new land

---
+++ land/it-cardiobalance/1/js/it.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        it: {
+            name: 'Italia',
+            code: 'it',
+            price: 39,
+            oldPrice: 78,
+            labelPrice: 'EUR',
+            phoneHelper: 'Per esempio, +39 0123456789',
+            nameHelper: 'Francesco Rossi',
+            nameError: 'Le nom obligatoire',
+            phoneError: 'Il telefono è obbligatorio',
+            countryError: 'Il nome del paese obbligatoria',
+            isDefault: true
+        }
+    };
+})();
