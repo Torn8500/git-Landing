adf3acf9a67b303e70e1a21ad94d431e493fe914
Torn8500 <quangpm.it@gmail.com>
2023-08-25 23:24:24 +0700

    js

--- land/pl-cardio-a/1/js/index.js
+++ land/pl-cardio-a/1/js/index.js
@@ -21,60 +21,26 @@ $(document).ready(function() {
     });
 
     Start();
-    // function Start() {
-    //     var m = 22,
-    //         s = 0;
-
-    //     if (m <= 9) {
-    //         m = "0" + m;
-    //     };
-
-    //     var timerId = setTimeout(function tick() {
-    //         if (s != 0) {
-    //             s = s - 1;
-
-    //             if (s <= 9) {
-    //                 s = "0" + s;
-    //             }
-    //         } else {
-    //             if (m != 0) {
-    //                 m = m - 1;
-    //                 s = 59;
-
-    //                 if (m <= 9) {
-    //                     m = "0" + m;
-    //                 }
-    //             } else {
-    //                 return
-    //             }
-    //         }
-
-    //         $('.form__time-count:nth-of-type(2) span:first-child').text(m);
-    //         $('.form__time-count:last-child span:first-child').text(s);
-    //         timerId = setTimeout(tick, 1000);
-    //     }, 1000);
-    // }
-
-    // Start();
-
     function Start() {
         const now = new Date();
-    
         const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59);
-    
-        const secondsRemaining = (endOfDay - now);
     
         setInterval(() => {
           const now = new Date();
           const secondsRemaining = Math.floor((endOfDay - now)/1000);
-    
-          const hours = Math.floor(secondsRemaining / 3600).toString().padStart(2, '0');
-          const minutes = Math.floor((secondsRemaining - (hours * 3600)) / 60).toString().padStart(2, '0');
-          const seconds = Math.floor(secondsRemaining % 60).toString().padStart(2, '0');
-    
-          $('.form__time-count:first-child span:first-child').text(hours);
-          $('.form__time-count:nth-of-type(2) span:first-child').text(minutes);
-          $('.form__time-count:last-child span:first-child').text(seconds);
+          if (secondsRemaining < 0) {
+            $('.form__time-count:first-child span:first-child').text("00");
+            $('.form__time-count:nth-of-type(2) span:first-child').text("00");
+            $('.form__time-count:last-child span:first-child').text("00");
+          } else {
+            const hours = Math.floor(secondsRemaining / 3600).toString().padStart(2, '0');
+            const minutes = Math.floor((secondsRemaining - (hours * 3600)) / 60).toString().padStart(2, '0');
+            const seconds = Math.floor(secondsRemaining % 60).toString().padStart(2, '0');
+      
+            $('.form__time-count:first-child span:first-child').text(hours);
+            $('.form__time-count:nth-of-type(2) span:first-child').text(minutes);
+            $('.form__time-count:last-child span:first-child').text(seconds);
+          }
         }, 1000);
       }
 
